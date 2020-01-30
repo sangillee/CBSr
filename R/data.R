e046f52a9868791e0b601027e4e6be4fc9ac8b85
Arthur Sangil Lee <sangillee3rd@gmail.com>
2020-01-29 19:32:40 -0500

    Getting package ready for deployment

--- R/data.R
+++ R/data.R
@@ -1,14 +1,27 @@
-#' Prices of 50,000 round cut diamonds.
+#' Sample participant data from a binary intertemporal choice task (aka delay discounting task)
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing one sample participant's 120 binary choices between a delayed monetary option (\code{Amt1} in \code{Delay1}) and a immediate monetary option ($20 now).
+#' The immediate monetary option was always '$20 now' across all trials
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 120 rows and 3 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
+#'   \item{Amt1}{Delayed reward amount, in dollars}
+#'   \item{Delay1}{Delay until the receipt of \code{Amt1}, in days}
+#'   \item{Choice}{Choice between binary options. \code{Choice==1} means participnat chose the delayed option (i.e., \code{Amt1} in \code{Delay1} days). \code{Choice==0} means participnat chose the immediate option (i.e., $20 now)}
 #' }
-#' @source \url{http://www.diamondse.info/}
+#' @source Kable, J. W., Caulfield, M. K., Falcone, M., McConnell, M., Bernardo, L., Parthasarathi, T., ... & Diefenbach, P. (2017). No effect of commercial cognitive training on brain activity, choice behavior, or cognitive performance. Journal of Neuroscience, 37(31), 7390-7402.
 "ITCdat"
 
+#' Sample participant data from a binary risky choice task (aka risk aversion task)
+#'
+#' A dataset containing one sample participant's 120 binary choices between a probabilistic monetary option (\code{Amt1} with \code{Prob1} chance of winning) and a certain monetary option ($20 for sure).
+#' The certain monetary option was always '$20 for sure' across all trials
+#'
+#' @format A data frame with 120 rows and 3 variables:
+#' \describe{
+#'   \item{Amt1}{Probabilistic reward amount, in dollars}
+#'   \item{Prob1}{Probability of winning \code{Amt1}, if it were to be chosen}
+#'   \item{Choice}{Choice between binary options. \code{Choice==1} means participnat chose the probabilistic option (i.e., \code{Amt1} with \code{Delay1} chance of winning). \code{Choice==0} means participnat chose the certain option (i.e., $20 for sure)}
+#' }
+#' @source Kable, J. W., Caulfield, M. K., Falcone, M., McConnell, M., Bernardo, L., Parthasarathi, T., ... & Diefenbach, P. (2017). No effect of commercial cognitive training on brain activity, choice behavior, or cognitive performance. Journal of Neuroscience, 37(31), 7390-7402.
+"RCdat"
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-## fit ITC data with 2-piece CBS function
+Package CBSr
+
+R Package for using Cubic Bezier Spline as a function approximator especially in modeling latent utility functions. While Cubic Bezier Splines (CBS) are heavily used in the graphics software industry, it can also be used as a flexible function approximation tool given the right constraints. The CBS package provides a method to calculate the y value from a x value given an appropriately constrained CBS curve. It then uses this method to approximate latent utility functions in intertemporal choice and risky choice data. Lee, Glaze, Bradlow, Kable, (2019) <doi:10.31234/osf.io/2ugwr>.
+
+
+
+Example code below:
+
+## fit example ITC data with 2-piece CBS function
 
 # each row is a choice between option 1 (Amt at Delay) vs option 2 (20 now).
 Amount1 = ITCdat$Amt1
@@ -21,7 +29,7 @@ lines(x,CBSfunc(out$xpos,out$ypos,x),col="black")
 
 
 
-## fit Risky choice data with 2-piece CBS function
+## fit example Risky choice data with 2-piece CBS function
 
 # each row is a choice between option 1 (Amt with prob) vs option 2 (20 for 100%).
 Amount1 = RCdat$Amt1
